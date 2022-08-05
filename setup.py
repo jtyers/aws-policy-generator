3f95b8ebfc6fd7c3316cf2c81aca9f259428f679
jtyers <40065647+jtyers@users.noreply.github.com>
2022-08-05 15:21:45 +0100

    release: 1.4.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="aws-policy-generator",
-    version="1.3.0",
+    version="1.4.0",
     description="AWS IAM Policy generator",
     long_description=README,
     long_description_content_type="text/markdown",
